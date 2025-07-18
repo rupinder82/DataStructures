d00a974f1287c56e524df2ab9d586948997674e7
cheema82 <cheemarupinder@gmail.com>
2025-07-18 16:05:06 -0700

    patterns basic

---
+++ src/main/java/NumberHalfPyramid.java
@@ -0,0 +1,15 @@
+package src.main.java;
+
+public class NumberHalfPyramid {
+
+    public static void main(String[] args){
+        int m = 5;
+        for(int i =1;i<=m;i++){
+            for(int j = 1; j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/ZeroOneTriangle.java
@@ -0,0 +1,18 @@
+package src.main.java;
+
+public class ZeroOneTriangle {
+    public static void main(String[] args){
+        int m = 5;
+        for(int i = 1; i<=m;i++){
+            for(int j = 1; j<=i;j++){
+                int sum = i+j;
+                if(sum%2==0){
+                    System.out.print("1");
+                }else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
